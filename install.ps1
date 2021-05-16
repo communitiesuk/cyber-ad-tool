4dbd9320a5d66257f4dd97fbf49e7488c82b108a
rob-brooks-layer7 <rob-brooks-layer7@users.noreply.github.com>
2021-05-16 10:04:24 +0000

    added install script

---
+++ install.ps1
@@ -0,0 +1,17 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-WindowsFeature RSAT-AD-PowerShell
+Install-Module -Name Pode
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+choco install nssm -y
+
+$exe = (Get-Command powershell.exe).Source
+$name = 'Active Directory Reporting Tool'
+$file = 'C:\cyber-ad-tool\server.ps1'
+$arg = "-ExecutionPolicy Bypass -NoProfile -Command `"$($file)`""
+
+
+nssm install $name $exe $arg
+nssm start $name
