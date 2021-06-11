691db4e80efd670cb154b3ba8fcfcfa82900011f
Tom Maguire <thomas@maguire.email>
2021-06-11 13:52:08 +0100

    Refactored code to meet ES6 guidance

--- public/javascript/ad.js
+++ public/javascript/ad.js
@@ -1,51 +1,54 @@
+/* jshint esversion: 6 */
 function includeHTML() {
-  var z, i, elmnt, file, xhttp;
   /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
+  const elements = document.getElementsByTagName("*");
+  for (let index = 0; index < elements.length; index++) {
+    const element = elements[index];
     /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
+    const includedFile = element.getAttribute("w3-include-html");
+    if (includedFile) {
       /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
+      const httpRequest = new XMLHttpRequest();
+      httpRequest.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 200) {
+            element.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            element.innerHTML = "Page not found.";
+          }
           /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
+          element.removeAttribute("w3-include-html");
         }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
+      };
+      httpRequest.open("GET", includedFile, true);
+      httpRequest.send();
     }
   }
 }
 
-
 function parseDate(date) {
-	try {
-		var utcSeconds =  parseInt((date).substring(6, 19));
-			parsedDate = new Date(utcSeconds); 
-	} catch {
-		parsedDate="";
-	}
-	return parsedDate;
+  let parsedDate;
+  try {
+    const utcSeconds = parseInt((date).substring(6, 19));
+    parsedDate = new Date(utcSeconds);
+  } catch (error) {
+    parsedDate = '';
+  }
+  return parsedDate;
 }
 
 function makeTextFile(text) {
-	var textFile = null;
-	var data = new Blob([text], {type: 'text/plain'});
-	// "data:text/csv;charset=utf-8," + 
-	// If we are replacing a previously generated file we need to
-	// manually revoke the object URL to avoid memory leaks.
-	if (textFile !== null) {
-	  window.URL.revokeObjectURL(textFile);
-	}
-	textFile = window.URL.createObjectURL(data);
-	return textFile;
+  let textFile = null;
+  const data = new Blob([text], {
+    type: 'text/plain'
+  });
+  // "data:text/csv;charset=utf-8," + 
+  // If we are replacing a previously generated file we need to
+  // manually revoke the object URL to avoid memory leaks.
+  if (textFile !== null) {
+    window.URL.revokeObjectURL(textFile);
+  }
+  textFile = window.URL.createObjectURL(data);
+  return textFile;
 }
